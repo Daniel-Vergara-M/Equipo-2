cc8cf4dc8c781b150c989020ee29ce5c9cb87ab8
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-05-02 18:35:56 -0500

    Refactor code in controller.h and menus.h to add functionality for searching people with a common destination

--- Pilas-Colas/view/menus.h
+++ Pilas-Colas/view/menus.h
@@ -5,29 +5,51 @@
  * @param p Persona a la que se le mostraran los viajes
  * @return void
  */
-void menu_principal(const persona &p) {
-    while (true) {
+void menu_principal(const persona &p)
+{
+    while (true)
+    {
         int option;
 
         cout << "Bienvenido " << p.getNombre_persona() << endl;
         cout << "1. Crear viaje" << endl;
         cout << "2. Ver viajes" << endl;
-        cout << "3. Salir" << endl;
+        cout << "4. Salir" << endl;
         cout << "Ingrese una opcion: ";
         cin >> option;
 
-        switch (option) {
-            case 1:
-                crear_viaje(const_cast<persona &>(p));
-                break;
-            case 2:
-                ver_viajes(p);
-                break;
-            case 3:
-                return;
-            default:
-                cout << "Opcion no valida" << endl;
-                break;
+        switch (option)
+        {
+        case 1:
+            crear_viaje(const_cast<persona &>(p));
+            break;
+        case 2:
+            ver_viajes(p);
+            break;
+        case 3:
+        {
+            destinos auxDestD;
+            string auxDest;
+            cout << "Ingrese el destino a buscar: ";
+            cin >> auxDest;
+            auxDestD.setNombre_destino(auxDest);
+            for (int i = 0; i < destiny.size(); i++)
+            {
+                if (destiny.top().getNombre_destino() == auxDest)
+                {
+                    verPersonasConDestino(destiny.top());
+                    break;
+                }
+                destiny.pop();
+            }
+            cout << "Destino no encontrado" << endl;
+            break;
+        }
+        case 4:
+            return;
+        default:
+            cout << "Opcion no valida" << endl;
+            break;
         }
     }
 }
@@ -36,15 +58,18 @@ void menu_principal(const persona &p) {
  * @brief Inicia sesion de un usuario. Posee una complejidad de O(n)
  * @return void
  */
-void iniciar_sesion() {
+void iniciar_sesion()
+{
 
     int long long id;
     cout << "ingrese su numero de identificacion: ";
     cin >> id;
 
-    while (!people.empty()) {
+    while (!people.empty())
+    {
         persona p = people.top();
-        if (p.getId_persona() == id) {
+        if (p.getId_persona() == id)
+        {
             menu_principal(p);
             return;
         }
@@ -57,8 +82,10 @@ void iniciar_sesion() {
  * @brief Muestra el menu inicial. Posee una complejidad de O(1)
  * @return void
  */
-void menu_inicial() {
-    while (true) {
+void menu_inicial()
+{
+    while (true)
+    {
         int option;
 
         cout << "1. Iniciar sesion" << endl;
@@ -66,15 +93,16 @@ void menu_inicial() {
         cout << "Ingrese una opcion: ";
         cin >> option;
 
-        switch (option) {
-            case 1:
-                iniciar_sesion();
-                break;
-            case 2:
-                return;
-            default:
-                cout << "Opcion no valida" << endl;
-                break;
+        switch (option)
+        {
+        case 1:
+            iniciar_sesion();
+            break;
+        case 2:
+            return;
+        default:
+            cout << "Opcion no valida" << endl;
+            break;
         }
     }
 }
